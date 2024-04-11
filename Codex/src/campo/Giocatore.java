1f2981c155387c7eb9cb098d9e0eada4e577b95c
megna <megna@air-di-megna.station>
2024-04-11 17:06:16 +0200

    Creazione Classe gioco per gestire i giocatori della partita
    attributi classe giocatori

--- Codex/src/campo/Giocatore.java
+++ Codex/src/campo/Giocatore.java
@@ -1,7 +1,29 @@
 package campo;
 
 public class Giocatore {
-	public static void main(String args[]) {
+	
+		//Attributi
+		private String nome;
+		int punti;
+
+		//Costrutto
+		public Giocatore(String nome) {
+			this.nome=nome;
+		}
 		
-	}
+		public String getNome() {
+			return nome;
+		}
+	
+		public String setNome() {
+			return nome;
+		}
+		
+		public String getPunti() {
+			return nome;
+		}
+		
+		public int calolaPunti() {
+			return punti;	
+		}
 }
---
+++ Codex/src/campo/Gioco.java
@@ -0,0 +1,34 @@
+package campo;
+
+import java.util.Scanner;
+
+public class Gioco {
+
+	//Creo scanner
+	Scanner sc = new Scanner(System.in);
+
+	private Giocatore[] giocatori;
+	private int Ngiocatori=0;
+	
+	//Costrutto
+	public Gioco() {
+		this.giocatori=new Giocatore[4];
+	}	
+	
+	public void inserisciGiocatore(String nome) {
+		
+		//Chiedo all'utente quando sono i giocatori 
+		System.out.println("giocatore: "+this.giocatori[1]+ " creato correttamente ");
+		Ngiocatori=sc.nextInt();
+		
+		for(int i=1; i<=Ngiocatori; i++)
+		{
+			if(this.giocatori[i]==null)
+			{
+				this.giocatori[0]=new Giocatore(nome);
+				System.out.println("giocatore: "+this.giocatori[0]+ " creato correttamente ");
+			}
+					
+		}
+	}
+}
