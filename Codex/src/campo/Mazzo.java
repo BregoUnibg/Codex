c7ffa89e9343ac66a43e5bc600ffbb3ed846b941
BregoUnibg <g.bregolin@studenti.unibg.it>
2024-05-01 21:43:10 +0200

    implementazione classe mazzo creaizone dei suoi metodi principali

--- Codex/src/campo/Mazzo.java
+++ Codex/src/campo/Mazzo.java
@@ -1,5 +1,38 @@
 package campo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import carte.Carta;
+
 public class Mazzo {
+	
+	private ArrayList <Carta> mazzo;
+	
+	public Mazzo() {
+		
+		mazzo = new ArrayList <Carta>();
+		
+	}
+	
+	//*Aggiungi una carta al mazzo;
+	public void add(Carta c) {
+		mazzo.add(c);
+	}
+	
+	public void mischia(){
+		Collections.shuffle(mazzo);
+	}
+	
+	public Carta pesca(){
+		
+		Carta cartaPescata = mazzo.get(0);
+		mazzo.remove(0);
+		return cartaPescata;
+		
+	}
+	
+	
+	
+	
 
 }
