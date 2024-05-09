40d09f74789a9390470d08014252778ef093a903
Gabriele <Gabriele@DESKTOP-JBKVC0D>
2024-05-09 19:10:45 +0200

    Implementazione Requisito Angoli coperti

---
+++ Codex/src/requisiti/ReqAngoliCoperti.java
@@ -0,0 +1,27 @@
+package requisiti;
+
+import campo.CampoGioco;
+import carte.Carta;
+
+public class ReqAngoliCoperti implements Requisito{
+
+	@Override
+	public int soddisfatto(CampoGioco c, Carta carta) {
+		
+		int conta = 0;
+		
+		
+		if(carta.getTop_left_angle().getAngolo_associato()!=null)
+			conta++;
+		if(carta.getTop_right_angle().getAngolo_associato()!=null)
+			conta++;
+		if(carta.getBottom_left_angle().getAngolo_associato()!=null)
+			conta++;
+		if(carta.getBottom_right_angle().getAngolo_associato()!=null)
+			conta++;
+		
+		return conta;
+	
+	}
+
+}
