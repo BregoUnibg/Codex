a4363b3257ec4709a2c9f8ade3c842e56b535095
Gabriele <Gabriele@DESKTOP-JBKVC0D>
2024-05-27 11:44:39 +0200

    Mofifica font visualizzazione mac

--- Codex/src/interfaccia/GBarraStatistiche.java
+++ Codex/src/interfaccia/GBarraStatistiche.java
@@ -34,14 +34,14 @@ public class GBarraStatistiche extends JPanel{
 		this.setLayout(new BorderLayout());
 		
 		this.nomeGiocatore = new JLabel(nomeGiocatore);
-		this.nomeGiocatore.setFont(new Font("Segoe UI Black", Font.PLAIN, 24));
+		this.nomeGiocatore.setFont(new Font("Segoe UI Black", Font.BOLD, 24));
 		this.nomeGiocatore.setBorder(new EmptyBorder(10, 20, 10, 20)); //Metto un po di bordo per non fare stare il nome attaccato alla parte sinistra della finestra
-		this.nomeGiocatore.setPreferredSize(new Dimension(500,300));
+		this.nomeGiocatore.setPreferredSize(new Dimension(400,300));
 		
 		this.descrizioneAzione = new JLabel();
-		this.descrizioneAzione.setFont(new Font("Segoe UI Black", Font.PLAIN, 16));
+		this.descrizioneAzione.setFont(new Font("Segoe UI Black", Font.BOLD, 16));
 		this.descrizioneAzione.setBorder(new EmptyBorder(10, 20, 10, 20)); //Metto un po di bordo per non fare stare il nome attaccato alla parte sinistra della finestra
-		this.descrizioneAzione.setPreferredSize(new Dimension(500,300));
+		this.descrizioneAzione.setPreferredSize(new Dimension(400,300));
 		
 		contatori = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		contatori.setBackground(this.getBackground());
@@ -87,7 +87,7 @@ public class GBarraStatistiche extends JPanel{
 	private void createContatore(JLabel contatore){
 		
 		contatore.setText("0");
-		contatore.setFont(new Font("Segoe UI Black", Font.PLAIN, 20));
+		contatore.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
 		contatore.setHorizontalTextPosition(SwingConstants.TRAILING);
         contatore.setVerticalTextPosition(SwingConstants.CENTER);
         contatore.setPreferredSize(new Dimension(70, 50));
