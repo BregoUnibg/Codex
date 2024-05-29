25ff81b625fbc37d6360946449e81690132c13af
Gabriele <Gabriele@DESKTOP-JBKVC0D>
2024-05-29 22:59:08 +0200

    Introduzione eccezione controllata

---
+++ Codex/src/interfaccia/GiocatoreNotFoundException.java
@@ -0,0 +1,12 @@
+package interfaccia;
+
+public class GiocatoreNotFoundException extends Exception{
+	
+	/**
+	 * Ecceczione manifestata quando un giocatore non viene trovato in un ipotetico elenco
+	 */
+	public GiocatoreNotFoundException(){
+		
+	}
+	
+}
